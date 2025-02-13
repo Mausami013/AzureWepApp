4926ce4d81c72cc4df7637a6398cde5e0085619d
kumarm13_grid <mausami.kumari@nationalgrid.com>
2025-02-13 15:13:39 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AzureWepApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
